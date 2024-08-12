525dcdaa43ae835620cfaaf82198b39e148e0c5f
erickbueno123 <erickbueno2007@gmail.com>
2024-08-12 23:09:28 +0000

    20/08/24-2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const idade = 17;
+let idade = 17;
 const anoNascimento = 2007;
 let anoAtual = 2024;
+const nomeCompleto = "Erick Daniel Bonora Bueno";
+
+console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
 
 anoAtual = anoAtual + 1;
-
 console.log(anoAtual);
+
+idade = idade + 1;
+console.log(idade);
